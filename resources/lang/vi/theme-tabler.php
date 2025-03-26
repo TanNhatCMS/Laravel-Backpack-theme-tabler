9240601fccc374841134001fbfe1ab771b8ee18e
TanNhatCMS <113796420+TanNhatCMS@users.noreply.github.com>
2025-03-26 23:23:16 +0700

    add language vietnamese

---
+++ resources/lang/vi/theme-tabler.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    // color mode
+    'color-mode-description' => 'Sử dụng chế độ màu :mode',
+    'color-mode-system' => 'hệ thống',
+    'color-mode-light' => 'sáng',
+    'color-mode-dark' => 'tối',
+
+    // password
+    'password-show' => 'Hiển thị mật khẩu',
+    'password-hide' => 'Ẩn mật khẩu',
+];
